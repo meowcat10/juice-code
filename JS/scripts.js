f8c91b86b1cea335f69ce0f7f1954cd8599fcf5f
meowcat10 <Andreas@mitkas.com>
2025-05-30 12:30:24 +0200

    Added banner and top-section

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+/* Definere mængde i kurven */
+const cartText = document.querySelector('.cart-text');
+
+/* Definere sektionen med kurv og dens mængde */
+const cart = document.querySelector('#cart');
+
+
